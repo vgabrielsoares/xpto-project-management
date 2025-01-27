c10b97b1e97b2a5b71b421ff99aebb7d0fcac2ff
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-01-27 18:30:11 -0300

    chore: base entities creation

---
+++ src/main/java/com/xpto/projectmanagment/domain/Project.java
@@ -0,0 +1,191 @@
+package com.xpto.projectmanagment.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.xpto.projectmanagment.domain.enumeration.ProjectStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A Project.
+ */
+@Entity
+@Table(name = "project")
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Project implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ProjectStatus status;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "project")
+    @JsonIgnoreProperties(value = { "project" }, allowSetters = true)
+    private Set<Task> tasks = new HashSet<>();
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Project id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Project name(String name) {
+        this.setName(name);
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Project description(String description) {
+        this.setDescription(description);
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return this.startDate;
+    }
+
+    public Project startDate(LocalDate startDate) {
+        this.setStartDate(startDate);
+        return this;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return this.endDate;
+    }
+
+    public Project endDate(LocalDate endDate) {
+        this.setEndDate(endDate);
+        return this;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public ProjectStatus getStatus() {
+        return this.status;
+    }
+
+    public Project status(ProjectStatus status) {
+        this.setStatus(status);
+        return this;
+    }
+
+    public void setStatus(ProjectStatus status) {
+        this.status = status;
+    }
+
+    public Set<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        if (this.tasks != null) {
+            this.tasks.forEach(i -> i.setProject(null));
+        }
+        if (tasks != null) {
+            tasks.forEach(i -> i.setProject(this));
+        }
+        this.tasks = tasks;
+    }
+
+    public Project tasks(Set<Task> tasks) {
+        this.setTasks(tasks);
+        return this;
+    }
+
+    public Project addTasks(Task task) {
+        this.tasks.add(task);
+        task.setProject(this);
+        return this;
+    }
+
+    public Project removeTasks(Task task) {
+        this.tasks.remove(task);
+        task.setProject(null);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Project)) {
+            return false;
+        }
+        return getId() != null && getId().equals(((Project) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        // see
+        // https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + getId() +
+                ", name='" + getName() + "'" +
+                ", description='" + getDescription() + "'" +
+                ", startDate='" + getStartDate() + "'" +
+                ", endDate='" + getEndDate() + "'" +
+                ", status='" + getStatus() + "'" +
+                "}";
+    }
+}
---
+++ src/main/java/com/xpto/projectmanagment/domain/Task.java
@@ -0,0 +1,153 @@
+package com.xpto.projectmanagment.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.xpto.projectmanagment.domain.enumeration.TaskStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+
+/**
+ * A Task.
+ */
+@Entity
+@Table(name = "task")
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Column(name = "deadline", nullable = false)
+    private Integer deadline;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private TaskStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = { "tasks" }, allowSetters = true)
+    private Project project;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Task id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Task title(String title) {
+        this.setTitle(title);
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Task description(String description) {
+        this.setDescription(description);
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDeadline() {
+        return this.deadline;
+    }
+
+    public Task deadline(Integer deadline) {
+        this.setDeadline(deadline);
+        return this;
+    }
+
+    public void setDeadline(Integer deadline) {
+        this.deadline = deadline;
+    }
+
+    public TaskStatus getStatus() {
+        return this.status;
+    }
+
+    public Task status(TaskStatus status) {
+        this.setStatus(status);
+        return this;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public Project getProject() {
+        return this.project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Task project(Project project) {
+        this.setProject(project);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Task)) {
+            return false;
+        }
+        return getId() != null && getId().equals(((Task) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        // see
+        // https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + getId() +
+                ", title='" + getTitle() + "'" +
+                ", description='" + getDescription() + "'" +
+                ", deadline=" + getDeadline() +
+                ", status='" + getStatus() + "'" +
+                "}";
+    }
+}
