4c2a3d065d4a9500e54df5acf95c60a7f1dd8b19
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-01-27 18:41:11 -0300

    chore: base entities DTO creation

---
+++ src/main/java/com/xpto/projectmanagment/service/dto/ProjectDTO.java
@@ -0,0 +1,113 @@
+package com.xpto.projectmanagment.service.dto;
+
+import com.xpto.projectmanagment.domain.enumeration.ProjectStatus;
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * A DTO for the {@link com.xpto.projectmanagment.domain.Project} entity.
+ */
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class ProjectDTO implements Serializable {
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @Lob
+    private String description;
+
+    @NotNull
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @NotNull
+    private ProjectStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public ProjectStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ProjectStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ProjectDTO)) {
+            return false;
+        }
+
+        ProjectDTO projectDTO = (ProjectDTO) o;
+        if (this.id == null) {
+            return false;
+        }
+        return Objects.equals(this.id, projectDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id);
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "ProjectDTO{" +
+                "id=" + getId() +
+                ", name='" + getName() + "'" +
+                ", description='" + getDescription() + "'" +
+                ", startDate='" + getStartDate() + "'" +
+                ", endDate='" + getEndDate() + "'" +
+                ", status='" + getStatus() + "'" +
+                "}";
+    }
+}
---
+++ src/main/java/com/xpto/projectmanagment/service/dto/TaskDTO.java
@@ -0,0 +1,112 @@
+package com.xpto.projectmanagment.service.dto;
+
+import com.xpto.projectmanagment.domain.enumeration.TaskStatus;
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A DTO for the {@link com.xpto.projectmanagment.domain.Task} entity.
+ */
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class TaskDTO implements Serializable {
+
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @Lob
+    private String description;
+
+    @NotNull
+    private Integer deadline;
+
+    @NotNull
+    private TaskStatus status;
+
+    private ProjectDTO project;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Integer deadline) {
+        this.deadline = deadline;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public ProjectDTO getProject() {
+        return project;
+    }
+
+    public void setProject(ProjectDTO project) {
+        this.project = project;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TaskDTO)) {
+            return false;
+        }
+
+        TaskDTO taskDTO = (TaskDTO) o;
+        if (this.id == null) {
+            return false;
+        }
+        return Objects.equals(this.id, taskDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id);
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "TaskDTO{" +
+                "id=" + getId() +
+                ", title='" + getTitle() + "'" +
+                ", description='" + getDescription() + "'" +
+                ", deadline=" + getDeadline() +
+                ", status='" + getStatus() + "'" +
+                ", project=" + getProject() +
+                "}";
+    }
+}
