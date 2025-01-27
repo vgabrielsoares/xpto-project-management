45fa430f4df8f4c6a2d468d94ad866caa2cfe6bb
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-01-27 18:31:50 -0300

    chore: enums creation

---
+++ src/main/java/com/xpto/projectmanagment/domain/enumeration/ProjectStatus.java
@@ -0,0 +1,11 @@
+package com.xpto.projectmanagment.domain.enumeration;
+
+/**
+ * The ProjectStatus enumeration.
+ */
+public enum ProjectStatus {
+    PLANNED,
+    IN_PROGRESS,
+    ABORTED,
+    COMPLETED,
+}
---
+++ src/main/java/com/xpto/projectmanagment/domain/enumeration/TaskStatus.java
@@ -0,0 +1,11 @@
+package com.xpto.projectmanagment.domain.enumeration;
+
+/**
+ * The TaskStatus enumeration.
+ */
+public enum TaskStatus {
+    PLANNED,
+    IN_PROGRESS,
+    ABORTED,
+    COMPLETED,
+}
