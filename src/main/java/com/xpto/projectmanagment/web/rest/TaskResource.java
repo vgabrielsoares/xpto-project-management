7f5812022fe431cca64809333b8e1b63d8055377
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-01-30 21:06:38 -0300

    chore: task resource formatting and clean up

--- src/main/java/com/xpto/projectmanagment/web/rest/TaskResource.java
+++ src/main/java/com/xpto/projectmanagment/web/rest/TaskResource.java
@@ -14,14 +14,10 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import tech.jhipster.web.util.HeaderUtil;
-import tech.jhipster.web.util.PaginationUtil;
 import tech.jhipster.web.util.ResponseUtil;
 
 /**
