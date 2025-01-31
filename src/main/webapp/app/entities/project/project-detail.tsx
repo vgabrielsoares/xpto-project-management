03b73b0b1b4e3e24dc2f0f99a38bddb471f16266
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-01-30 21:40:17 -0300

    style: detail pages stylization and update

--- src/main/webapp/app/entities/project/project-detail.tsx
+++ src/main/webapp/app/entities/project/project-detail.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { Button, Col, Row } from 'reactstrap';
+import { Button, Col, Row, Container } from 'reactstrap';
 import { TextFormat } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -20,50 +20,87 @@ export const ProjectDetail = () => {
 
   const projectEntity = useAppSelector(state => state.project.entity);
   return (
-    <Row>
-      <Col md="8">
-        <h2 data-cy="projectDetailsHeading">Project</h2>
-        <dl className="jh-entity-details">
-          <dt>
-            <span id="id">ID</span>
-          </dt>
-          <dd>{projectEntity.id}</dd>
-          <dt>
-            <span id="name">Name</span>
-          </dt>
-          <dd>{projectEntity.name}</dd>
-          <dt>
-            <span id="description">Description</span>
-          </dt>
-          <dd>{projectEntity.description}</dd>
-          <dt>
-            <span id="startDate">Start Date</span>
-          </dt>
-          <dd>
-            {projectEntity.startDate ? <TextFormat value={projectEntity.startDate} type="date" format={APP_LOCAL_DATE_FORMAT} /> : null}
-          </dd>
-          <dt>
-            <span id="endDate">End Date</span>
-          </dt>
-          <dd>{projectEntity.endDate ? <TextFormat value={projectEntity.endDate} type="date" format={APP_LOCAL_DATE_FORMAT} /> : null}</dd>
-          <dt>
-            <span id="status">Status</span>
-          </dt>
-          <dd>{projectEntity.status}</dd>
-          <dt>
-            <span id="team">Team</span>
-          </dt>
-          <dd>{projectEntity.team}</dd>
-        </dl>
-        <Button tag={Link} to="/project" replace color="info" data-cy="entityDetailsBackButton">
-          <FontAwesomeIcon icon="arrow-left" /> <span className="d-none d-md-inline">Back</span>
-        </Button>
-        &nbsp;
-        <Button tag={Link} to={`/project/${projectEntity.id}/edit`} replace color="primary">
-          <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
-        </Button>
-      </Col>
-    </Row>
+    <Container className="project-detail">
+      <Row className="detail-row">
+        <Col md="8">
+          <h2 data-cy="projectDetailsHeading" className="detail-title">
+            Project Detail
+          </h2>
+          <dl className="jh-entity-details">
+            <Row className="detail-item">
+              <Col md="6">
+                <dt>ID</dt>
+              </Col>
+              <Col md="6">
+                <dd>{projectEntity.id}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="6">
+                <dt>Name</dt>
+              </Col>
+              <Col md="6">
+                <dd>{projectEntity.name}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="6">
+                <dt>Description</dt>
+              </Col>
+              <Col md="6">
+                <dd>{projectEntity.description}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="6">
+                <dt>Start Date</dt>
+              </Col>
+              <Col md="6">
+                <dd>
+                  {projectEntity.startDate ? (
+                    <TextFormat value={projectEntity.startDate} type="date" format={APP_LOCAL_DATE_FORMAT} />
+                  ) : null}
+                </dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="6">
+                <dt>End Date</dt>
+              </Col>
+              <Col md="6">
+                <dd>
+                  {projectEntity.endDate ? <TextFormat value={projectEntity.endDate} type="date" format={APP_LOCAL_DATE_FORMAT} /> : null}
+                </dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="6">
+                <dt>Status</dt>
+              </Col>
+              <Col md="6">
+                <dd>{projectEntity.status}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="6">
+                <dt>Team</dt>
+              </Col>
+              <Col md="6">
+                <dd>{projectEntity.team}</dd>
+              </Col>
+            </Row>
+          </dl>
+          <div className="button-group">
+            <Button tag={Link} to="/project" replace color="info" data-cy="entityDetailsBackButton">
+              <FontAwesomeIcon icon="arrow-left" /> <span className="d-none d-md-inline">Back</span>
+            </Button>
+            <Button tag={Link} to={`/project/${projectEntity.id}/edit`} replace color="primary">
+              <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/main/webapp/app/entities/task/task-detail.tsx
+++ src/main/webapp/app/entities/task/task-detail.tsx
@@ -1,16 +1,12 @@
 import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { Button, Col, Row } from 'reactstrap';
-
+import { Button, Col, Row, Container } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { useAppDispatch, useAppSelector } from 'app/config/store';
-
 import { getEntity } from './task.reducer';
 
 export const TaskDetail = () => {
   const dispatch = useAppDispatch();
-
   const { id } = useParams<'id'>();
 
   useEffect(() => {
@@ -19,46 +15,81 @@ export const TaskDetail = () => {
 
   const taskEntity = useAppSelector(state => state.task.entity);
   return (
-    <Row>
-      <Col md="8">
-        <h2 data-cy="taskDetailsHeading">Task</h2>
-        <dl className="jh-entity-details">
-          <dt>
-            <span id="id">ID</span>
-          </dt>
-          <dd>{taskEntity.id}</dd>
-          <dt>
-            <span id="title">Title</span>
-          </dt>
-          <dd>{taskEntity.title}</dd>
-          <dt>
-            <span id="description">Description</span>
-          </dt>
-          <dd>{taskEntity.description}</dd>
-          <dt>
-            <span id="deadline">Deadline</span>
-          </dt>
-          <dd>{taskEntity.deadline}</dd>
-          <dt>
-            <span id="status">Status</span>
-          </dt>
-          <dd>{taskEntity.status}</dd>
-          <dt>
-            <span id="responsible">Responsible</span>
-          </dt>
-          <dd>{taskEntity.responsible}</dd>
-          <dt>Project</dt>
-          <dd>{taskEntity.project ? taskEntity.project.name : ''}</dd>
-        </dl>
-        <Button tag={Link} to="/task" replace color="info" data-cy="entityDetailsBackButton">
-          <FontAwesomeIcon icon="arrow-left" /> <span className="d-none d-md-inline">Back</span>
-        </Button>
-        &nbsp;
-        <Button tag={Link} to={`/task/${taskEntity.id}/edit`} replace color="primary">
-          <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
-        </Button>
-      </Col>
-    </Row>
+    <Container className="task-detail">
+      <Row className="detail-row">
+        <Col md="8">
+          <h2 data-cy="taskDetailsHeading" className="detail-title">
+            Task Detail
+          </h2>
+          <dl className="jh-entity-details">
+            <Row className="detail-item">
+              <Col md="4">
+                <dt>ID</dt>
+              </Col>
+              <Col md="8">
+                <dd>{taskEntity.id}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="4">
+                <dt>Title</dt>
+              </Col>
+              <Col md="8">
+                <dd>{taskEntity.title}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="4">
+                <dt>Description</dt>
+              </Col>
+              <Col md="8">
+                <dd>{taskEntity.description}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="4">
+                <dt>Deadline</dt>
+              </Col>
+              <Col md="8">
+                <dd>{taskEntity.deadline}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="4">
+                <dt>Status</dt>
+              </Col>
+              <Col md="8">
+                <dd>{taskEntity.status}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="4">
+                <dt>Responsible</dt>
+              </Col>
+              <Col md="8">
+                <dd>{taskEntity.responsible}</dd>
+              </Col>
+            </Row>
+            <Row className="detail-item">
+              <Col md="4">
+                <dt>Project</dt>
+              </Col>
+              <Col md="8">
+                <dd>{taskEntity.project ? taskEntity.project.name : ''}</dd>
+              </Col>
+            </Row>
+          </dl>
+          <div className="button-group">
+            <Button tag={Link} to="/task" replace color="info" data-cy="entityDetailsBackButton">
+              <FontAwesomeIcon icon="arrow-left" /> <span className="d-none d-md-inline">Back</span>
+            </Button>
+            <Button tag={Link} to={`/task/${taskEntity.id}/edit`} replace color="primary">
+              <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
